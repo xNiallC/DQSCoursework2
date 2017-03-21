2a22e6a721ad0f87fa65516d85160a0e4e32547d
Niall Curtis <niall@pc-78-31.customer.arofanhouse.studenthousingcompany.lan>
2017-03-21 12:48:59 +0000

    Shit loads of comments

--- FindingStuff.py
+++ FindingStuff.py
@@ -13,17 +13,33 @@ def assignTutor(tutorList):
 
         newTempDict = tutorList
 
+        # This is where it got harder.
+        # The while loop checks that the list of available tutors is not 0, which we come to later.
         while len(newTempDict) != 0:
+
+            # Pick random tutor from list
             randomTutor = random.choice(list(tutorList.items()))
+            # Initialise variable for tutor quote
             numberOfStudentsWithTutor = 0
+
+            # For each row in the student csv, if they currently have the randomly selected tutor, add 1 to the variable.
             for row in reader:
                 if row[4] == randomTutor[0]:
                     numberOfStudentsWithTutor += 1
+
+            # We now compare the variable with the tutor's quota number in the CSV.
+            # If this number is equal to or exceeds their quota, we delete them from the list.
+            # Else the while loop goes again until their are no tutors left.
             if numberOfStudentsWithTutor >= int(randomTutor[1]):
                 del newTempDict[randomTutor[0]]
             else:
+            # TODO: Nothing is actually written to the csv yet, I just completed the algorithm. Next step is to write the tutor name to the CSV.
                 print("success")
                 break
+
+            # TODO: I'm not sure what she wants to happen assuming there are no tutors left at all.
+            # Maybe it just straight up fails and the student is fucked? Who knows.
+
 
 
 def returnStudent(*args):
@@ -38,31 +54,45 @@ def returnStudent(*args):
             action = comboValue.get()
 
             if action == "Get Info":
+                # Searching for information
                 for row in reader:
+                    # Returns full info if you know a first/second name
                     if (nameinput == row[0].lower()) or (nameinput == row[1].lower()):
                         answer1.set("--> " + row[0] + " " + row[1] + " " + row[2])
                         break
+                    # Returns a name if you know a number
                     elif nameinput == row[2]:
                         answer1.set("--> " + row[0] + " " + row[1])
                         break
+                    # Returns a nope if you know nothing
                     else:
                         answer1.set("--> " + "Student not found.")
 
             if action == "Assign Student":
+                # Initialise variables
                 tempList = {}
                 tempStudent = ""
+
+                # Find a student from a number and assign its subject to the tempStudent variable
                 for row in reader:
                     if (nameinput == row[2]):
                         tempStudent = row[3]
                     else:
                         answer1.set("--> Student Not Found")
                         break
+                # Here it gets more complex. We earlier created a temporary dict.
+                # Now we iterate through the csv of tutor, and if the tutor's subject matches the student's
+                # we add the tutor's full name and quota number to the dict.
                 for row in tutorReader:
                     if tempStudent == row[3]:
                         tempList[row[0] + " " + row[1]] = row[4]
+
+                # If there were no tutors matching the student's subject,  we add all the tutors to the list.
+                # Else, run the function with list of matched tutors.
                 if len(tempList) == 0:
                     for row in tutorReader:
                         tempList[row[0] + " " + row[1]] = row[4]
+                    # Run assigning function
                     assignTutor(tempList)
                 else:
                     assignTutor(tempList)
